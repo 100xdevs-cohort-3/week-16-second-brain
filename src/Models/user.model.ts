d0b2225fb42423f342b8c41b1b0fc345e3f04c66
Shubrajit <shubrajitdeb180603@gmail.com>
2024-11-16 21:28:23 +0530

    Models created

---
+++ src/Models/user.model.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: [true, "email is required"],
+  },
+  password: {
+    type: String,
+    required: [true, "password is required"],
+    lowercase: true,
+  },
+} ,{timestamps:true});
+
+export const  User = mongoose.model("User",userSchema)
